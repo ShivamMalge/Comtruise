d4673f760ace3fefd103db4cbd007776b4213698
ShivamMalge <147587521+ShivamMalge@users.noreply.github.com>
2025-02-28 19:34:49 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
-        output: "export",
-    },
+  output: "export", // Enables static export for GitHub Pages
+  basePath: "/ComTruise", // Set to your GitHub repo name
+  images: {
+    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+    unoptimized: true, // Required for static deployments
+  },
 };
 
 export default nextConfig;
